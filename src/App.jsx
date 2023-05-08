b6a491cde9dfb6a460c5377241dccad674f131d9
Maurizio <mauriziodm91@gmail.com>
2023-05-08 15:03:56 -0300

    created Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import Hero from './components/Hero/Hero.component'
+import Demo from './components/Demo/Demo.component' //TO DO
+
+import './App.css'
+
 const App = () => {
-  return <div>App</div>
+  return (
+    <main>
+      <div className='main'>
+        <div className='gradient'></div>
+      </div>
+
+      <div className='app'>
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  )
 }
 
 export default App
