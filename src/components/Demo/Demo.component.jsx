97564e82f78376a0f54a3d0ae46059130ed74f0a
Maurizio <mauriziodm91@gmail.com>
2023-05-09 22:11:09 -0300

    refactored Demo component

--- src/components/Demo/Demo.component.jsx
+++ src/components/Demo/Demo.component.jsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react'
 import { copy, linkIcon, loader, tick } from '../../assets'
+import { useLazyGetSummaryQuery } from '../../services/article'
+import UrlBar from '../Urlbar/UrlBar.component'
+import SummaryDisplay from '../SummaryDisplay/SummaryDisplay.component'
+import LinkTab from '../LinkTab/LinkTab.component'
 
 const Demo = () => {
   const [article, setArticle] = useState({
@@ -7,36 +11,66 @@ const Demo = () => {
     summary: '',
   })
 
-  const handleSubmit = async (e) => alert('submitted')
+  const [allArticles, setAllArticles] = useState([])
+
+  const [copied, setCopied] = useState('')
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem('articles')
+    )
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage)
+    }
+  }, [])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const { data } = await getSummary({ url: article.url })
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary }
+      const updatedNewArticles = [newArticle, ...allArticles]
+
+      setArticle(newArticle)
+      setAllArticles(updatedNewArticles)
+
+      localStorage.setItem('articles', JSON.stringify(updatedNewArticles))
+    }
+  }
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl)
+    navigator.clipboard.writeText(copyUrl)
+    setTimeout(() => setCopied(''), 3000)
+  }
+
+  const urlBarProps = { handleSubmit, linkIcon, article, setArticle }
+  const summaryDisplayProps = { isFetching, error, loader, article }
 
   return (
     <section className='mt-16 w-full max-w-xl'>
       <div className='flex flex-col w-full gap-2'>
-        <form
-          className='relative flex justify-center items-center'
-          onSubmit={handleSubmit}
-        >
-          <img
-            src={linkIcon}
-            alt='link-icon'
-            className='absolute left-0 my-2 ml-3 w-5'
-          />
-          <input
-            type='url'
-            placeholder='Enter a URL'
-            value={article.url}
-            onChange={(e) => setArticle({ ...article, url: e.target.value })}
-            required
-            className='url_input peer'
-          />
-          <button
-            type='submit'
-            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'
-          >
-            ↵
-          </button>
-        </form>
+        <UrlBar {...urlBarProps} />
+
+        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+          {allArticles.map((art, index) => (
+            <LinkTab
+              key={`link-${index}`}
+              index={index}
+              setArticle={setArticle}
+              article={art}
+              copy={copy}
+              handleCopy={handleCopy}
+              copied={copied}
+              tick={tick}
+            />
+          ))}
+        </div>
       </div>
+      <SummaryDisplay {...summaryDisplayProps} />
     </section>
   )
 }
